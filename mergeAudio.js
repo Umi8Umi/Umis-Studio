b0d94297ca722df7e902f02952001fa3264d5585
Sarah Leventhal <sul2102@barnard.edu>
2021-01-18 02:49:51 -0500

    added a bit of an animation thing

--- mergeAudio.js
+++ mergeAudio.js
@@ -7,6 +7,9 @@ document.addEventListener("DOMContentLoaded", function(event) {
   var dest = audioCtx.createMediaStreamDestination();
   var mediaRecorder = new MediaRecorder(dest.stream);
 
+  //const theBand = document.getElementById('theBand');
+  const theBand = document.getElementById('band-animation');
+
   //////////////////////////////////////////////////////
   //
   // originally this was going to be a merge function
@@ -20,10 +23,44 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
   const mergeBtn = document.getElementById('merge');
   mergeBtn.addEventListener('click', function() {
+    startAnimation();
     soundBites = Array.from(document.getElementsByTagName('audio'));
     soundBites.forEach(function(audio) {
       audio.play();
+      /*audio.onended(stopAnimation);*/
+      audio.onended = function() {
+          stopAnimation();
+      };
     });
+      
+/*    if(!soundBites[0].paused){
+      startAnimation();
+    }
+    else
+      stopAnimation();*/
+/*    if(!soundBites[0].paused){
+      setTimeout(function(){ theBand.src = "band2.PNG"; }, 0);
+      setTimeout(function(){ theBand.src = "band1.PNG"; }, 500);
+    }*/
   }, false);
+
+  var animating = false;
+  var animateGo;
+  function startAnimation() { 
+    animating = true;
+    var frames = document.getElementById("band-animation").children;
+    var frameCount = frames.length;
+    var i = 0;
+    animateGo = setInterval(function () { 
+      frames[i % frameCount].style.display = "none";
+      frames[++i % frameCount].style.display = "block";
+    }, 500);
+  }
+
+  function stopAnimation() {
+    console.log("i am here");
+    animating = false;
+    clearInterval(animateGo);
+  }
 
 }, false);
